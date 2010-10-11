4ae4848ae521eec70dcbc187494832eb471f531d
GarrenSmith <garren.smith@gmail.com>
2010-10-11 18:33:27 +0000

    added post url

--- build_machine.js
+++ build_machine.js
@@ -4,41 +4,46 @@
  */
 
 var express = require('express');
+var inspect = require('sys').inspect;
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use(express.bodyDecoder());
-    app.use(express.methodOverride());
-    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-    app.use(app.router);
-    app.use(express.staticProvider(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.use(express.bodyDecoder());
+  app.use(express.methodOverride());
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-   app.use(express.errorHandler()); 
+  app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res){
-    res.render('index.jade', {
-        locals: {
-            title: 'Build Machine'
-        }
-    });
+  res.render('index.jade', {
+    locals: {
+              title: 'Build Machine'
+            }
+  });
+});
+
+app.post('/url',function(req,res){
+  console.log(req.body.git_url);
 });
 
 // Only listen on $ node app.js
 
 if (!module.parent) {
-    app.listen(3000);
-    console.log("Express server listening on port %d", app.address().port)
+  app.listen(3000);
+  console.log("Express server listening on port %d", app.address().port)
 }
