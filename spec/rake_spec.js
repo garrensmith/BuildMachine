86e56bddf2f9710085e95bed259a1ad9937fdfc2
GarrenSmith <garren.smith@gmail.com>
2010-10-27 14:25:05 +0000

    rake complete for now

--- spec/rake_spec.js
+++ spec/rake_spec.js
@@ -1,46 +1,83 @@
 var describe = require('spec_my_node').describe,
-  child_process = require('child_process'),
-  inspect = require('sys').inspect;
- 
+    child_process = require('child_process'),
+    inspect = require('sys').inspect;
+
 var spawnMock = child_process.spawn = function (cmd,args,options) {
   spawnMock.cmd = cmd;
   spawnMock.args = args;
   spawnMock.optins = options;
   spawnMock.called = true;
+  return spawnMock;
 };
+
+spawnMock_stdout = function () {
+};
+
+spawnMock_stdout.on = function(eventName, fn) {
+  this.eventName = eventName;
+};
+
+spawnMock_stderr = function () {
+};
+
+spawnMock_stderr.on = function(eventName, fn) {
+  this.eventName = eventName;
+};
+
+
+
+spawnMock.stdout = spawnMock_stdout;
+spawnMock.stderr = spawnMock_stderr; 
 
 rake = require('../lib/rake');
 
 
 describe("Rake params").
   it("Should create child process with correct commands", function () {
-    rake.run("directory");
-    child_process.spawn.cmd.should().beEqual('rake');
-  }).
+  rake.run("directory");
+  child_process.spawn.cmd.should().beEqual('rake');
+}).
   it("Should run default if no task given", function () {
-    rake.run("directory");
-    spawnMock.args.should().beEqual('default');
-  }).
+  rake.run("directory");
+  spawnMock.args.should().beEqual('default');
+}).
   it("Should run with supplied command", function () {
-    rake.run("directory", "spec");
-    spawnMock.args.should().beEqual("spec");
-  });
-/*  it("Should create child process", function () {
-    var mockCalled = false;
-        
-    spawnMock = function(cmd, args, options) {
-      cmd.should().beEqual(cmd);
-      args.should().contain('default');
-      options.cwd.should().beEqual(directory);
-    }
-    
-    rake.run("directory");
-    mockCalled.should().beTrue();
+  rake.run("directory", "spec");
+  spawnMock.args.should().beEqual("spec");
+});
+
+describe("Std out setup").
+  it("Should setup std out data event", function () {
+  rake.run("fake");
+
+  spawnMock_stdout.eventName.should().beEqual('data');
+
+  }).
+  it("Should setup std err data event", function () {
+  rake.run("fake");
+
+  spawnMock_stderr.eventName.should().beEqual('data');
+
   });
 
-describe("Failed Command").
-  it("Should return error", function () {
-    //pending
-    var num = 1;
-    num.should().beEqual(2);
-  });*/
+
+
+/*  it("Should create child process", function () {
+    var mockCalled = false;
+
+    spawnMock = function(cmd, args, options) {
+    cmd.should().beEqual(cmd);
+    args.should().contain('default');
+    options.cwd.should().beEqual(directory);
+    }
+
+    rake.run("directory");
+    mockCalled.should().beTrue();
+    });
+
+    describe("Failed Command").
+    it("Should return error", function () {
+//pending
+var num = 1;
+num.should().beEqual(2);
+});*/
