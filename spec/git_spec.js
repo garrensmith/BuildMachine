1d6ad7835b983da507c00a7ac5ab3058b1bdbff7
GarrenSmith <garren.smith@gmail.com>
2010-12-11 17:19:08 +0200

    moved and setup new git lib

--- spec/git_spec.js
+++ spec/git_spec.js
@@ -1,81 +1,64 @@
 var describe = require('Jody').describe,
-    child_process = require('child_process'),
-    inspect = require('sys').inspect;
+    fs = require('fs'),
+    path = require('path'),
+    spawn = require('child_process').spawn,
+    Git = require('../lib/git.js');
 
- var spawn = child_process.spawn = function(cmd,arg,opt){
-      spawn.arg = arg;
-      spawn.opt = opt;
-      spawn.cmd = cmd;
-      spawn.onEventName;
-      spawn.stdoutEventName;
-      spawn.stderrEventName;
 
-      spawn.on = function (eventName) {
-        spawn.onEventName = eventName;
-        return spawn;
-      };
+
+var url = "/Users/garren/WebDev/Jody/",
+    folderLocation = "./tmp",
+    folderName = "testrepo" ;
+
+//implement beforeAll and AfterAll to cleanup and setup tests
+
+describe('Git Clone').
+  beforeEach(function () {
+   /* if (!path.exists("./tmp")) {
+      fs.mkdirSync("./tmp");
+    }*/
+    //var tt = spawn('rm', ["-rf", "./tmp/testrepo"]); 
+
       
-      spawn.stdout = function () {
-        
-      }
-
-      spawn.stdout.on = function (eventName) {
-        spawn.stdoutEventName = eventName
-      }; 
-
-      spawn.stderr = function () {
-        
-      }
-
-      spawn.stderr.on = function (eventName) {
-        spawn.stderrEventName = eventName
-      }; 
-
-
-      return spawn;
-  };
-
-
-var git = require('../lib/git');
-
-describe('Child process').
-  it("Should be called with git command", function () {
-      git.clone("","", "");
-      
-      spawn.cmd.should().beEqual('git');
-
+    //}
   }).
-  it("Should setup exit event", function () {
-    git.clone("","", function () {});
-  
-    spawn.onEventName.should().beEqual('exit');
-  });
- 
-describe('Git outputs').
-  it("Should register for stdout event", function () {
-     git.clone("","", "");
-
-     spawn.stdoutEventName.should().beEqual('data');
+  it("Should clone into specified folder", function (atEnd) {
+    
+    var git = new Git();
+    git.clone(url, folderLocation, folderName, function () {});    
+    
+    atEnd(function () {
+      var files = fs.readdirSync(folderLocation + "/" + folderName)
+      files.length.should().beEqual(10);
+    });
+}).
+  it("Should execute call back on clone completion", function (atEnd) {
+    var called = false;
+    var git = new Git();
+    git.clone(url, folderLocation, folderName, function () {
+      called = true;
+    });    
+    
+    atEnd(function () {
+      called.should().beTrue;
+    });
   }).
-  it("Should register for stderr event", function () {
-     git.clone("","", "");
-     spawn.stderrEventName.should().beEqual('data');
+  it("Should fire call back on information", function (atEnd) {
+    var called = false;
+    var git= new Git();
+    
+    git.clone(url, folderLocation, folderName, function () {
+      called = true;
+    });
+
+    git.on('data', function () {
+      called = true;
+    });
+
+    atEnd(function () {
+      called.should().beTrue;
+    });
   });
-
-describe('Clone arguments').
-  it("Should add clone directory if specified", function () {
-    git.clone("url","clone directory","folderName", function () {}, function () {});
-
-    spawn.arg.should().contain("folderName");
-  }).
-  it("Should ignore clone folder if ignores", function () {
-    git.clone("url","clone directory",function () {});
-
-    spawn.arg.length.should().beEqual(2);
-  });
-
-
-
 
 
 
